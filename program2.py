83bf17dbf4c4c1ae0dc0df0947e131e2540a6df3
DJMistry02 <50930503+DJMistry02@users.noreply.github.com>
2024-05-08 11:02:11 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,4 +1,4 @@
-# maybe check the indexes in both strings and check for values.. even if one possible true, return true. use dp to keep track
+# maybe check the indexes in both strings and check for values.. even if one possible true, return true. use dp to keep track of upto which indexes it has been calculated
 
 def decode_message( s: str, p: str) -> bool:
 

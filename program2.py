a5008cb67d01a2415795ea5d329c38f0859ba37f
DJMistry02 <50930503+DJMistry02@users.noreply.github.com>
2024-05-08 14:50:10 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,22 +1,3 @@
-# maybe check the indexes in both strings and check for values.. even if one possible true, return true. use dp to keep track of upto which indexes it has been calculated
-
-def checkMatch(sIndex, pIndex,sLength,pLength,s,p,dp):
-    # if already calculated, return that value
-    if dp[sIndex][pIndex] != -1:
-        return dp[sIndex][pIndex]
-    ans = False
-    sChar = s[sIndex]
-    pChar = p[pIndex]
-    #check characters by index with special cases for other characters. for ? just skip charcter and for * check if we can go to the next character
-    
-
-
-
 def decode_message( s: str, p: str) -> bool:
-    sLength = len(s)
-    pLength = len(p)
-
-    dp = [[[-1] * sLength] for _ in range(pLength)]
-
-
+    if s==
     return checkMatch(0,0,sLength,pLength,s,p,dp)

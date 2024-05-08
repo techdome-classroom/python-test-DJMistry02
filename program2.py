51fa7efb03448cb52f5a61fa13121590de63a37b
DJMistry02 <50930503+DJMistry02@users.noreply.github.com>
2024-05-08 11:10:48 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -7,7 +7,7 @@ def checkMatch(sIndex, pIndex,sLength,pLength,s,p,dp):
     ans = False
     sChar = s[sIndex]
     pChar = p[pIndex]
-    nextChar = pIndex<
+    nextChar = pIndex<pLength-1 ? 
     if pChar == '?':
 
 

b7422d1de217bb4b6faa9c372ec51fefb025833c
DJMistry02 <50930503+DJMistry02@users.noreply.github.com>
2024-05-08 11:05:13 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,6 +1,6 @@
 # maybe check the indexes in both strings and check for values.. even if one possible true, return true. use dp to keep track of upto which indexes it has been calculated
 
-
+def checkMatch(sIndex, pIndex,)
 
 
 def decode_message( s: str, p: str) -> bool:

044f8891f253b2122e19f0c1f63a9acea64788af
DJMistry02 <50930503+DJMistry02@users.noreply.github.com>
2024-05-08 11:01:44 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,3 +1,5 @@
+# maybe check the indexes in both strings and check for values.. even if one possible true, return true. use dp to keep track
+
 def decode_message( s: str, p: str) -> bool:
 
         

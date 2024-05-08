79b933faa75a969649d9f439ddcf43c75a9f640e
DJMistry02 <50930503+DJMistry02@users.noreply.github.com>
2024-05-08 11:09:38 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,7 +1,7 @@
 # maybe check the indexes in both strings and check for values.. even if one possible true, return true. use dp to keep track of upto which indexes it has been calculated
 
 def checkMatch(sIndex, pIndex,sLength,pLength,s,p,dp):
-    # if already calculated, return 
+    # if already calculated, return that value
     if dp[sIndex][pIndex] != -1:
         return dp[sIndex][pIndex]
     ans = False
